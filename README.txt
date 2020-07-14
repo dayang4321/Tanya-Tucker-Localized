e907bf65dab5c2fe1145ab172600c203d3d40b76
dayang4321 <dayang4321@users.noreply.github.com>
2020-07-14 01:00:45 +0100

    first

---
+++ README.txt
@@ -0,0 +1,8 @@
+Material Design for Bootstrap
+Version: MDB FREE 4.19.1
+
+Documentation:
+https://mdbootstrap.com/
+
+
+
---
+++ assets/js/custom/custom.js
@@ -0,0 +1,7 @@
+
+//Loading Fuction
+
+window.onload = (event) => {
+    document.getElementById("loading").style.display = "none";
+    document.querySelector(".loadable").style.display = "block";
+}
